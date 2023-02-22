91dd9c11ba17c741cc9466ac87f9912e94a32b8c
0pin0 <67333689+0pin0@users.noreply.github.com>
2023-02-22 16:23:12 +0000

    install phaser, create new game

--- Example.ts
+++ Example.ts
@@ -1,55 +1,28 @@
-class Example extends Phaser.Scene {
-    constructor() {
-        super();
-    }
+import { Game, Scene } from "phaser";
 
-    preload() {
-        this.load.spritesheet('diamonds', 'assets/sprites/diamonds32x5.png', { frameWidth: 64 });
-    }
-
-    create() {
-        //  Create 108 sprites in a grid so we can see them vanish easily
-        const group = this.make.group({
-            key: 'diamonds',
-            frame: [0, 1, 2, 3, 4],
-            frameQuantity: 22,
-            max: 108
-        });
-
-        Phaser.Actions.GridAlign(group.getChildren(), {
-            width: 12,
-            height: 9,
-            cellWidth: 64,
-            cellHeight: 64,
-            x: 48,
-            y: 32
-        });
-
-        //  Remove one child from the display list every half a second
-        const timedEvent = this.time.addEvent({
-            delay: 500,
-            callback: this.onEvent,
-            callbackScope: this,
-            loop: true
-        });
-    }
-
-    onEvent() {
-        const child = this.children.getRandom();
-        if (child) {
-            this.children.remove(child);
-        }
-    }
-}
-
-const config = {
-    type: Phaser.WEBGL,
+// Define your game configuration
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO, // Change to Phaser.CANVAS, Phaser.WEBGL, or Phaser.HEADLESS
     width: 800,
     height: 600,
-    backgroundColor: '#2d2d2d',
-    parent: 'phaser-example',
-    pixelArt: true,
-    scene: [Example]
+    parent: "game",
+    scene: [], // Add custom scenes to the array
+    physics: {
+        default: "arcade"
+    },
+    backgroundColor: "#000000"
 };
 
-const game = new Phaser.Game(config);
+// Create a new game instance based on the configuration
+const game: Phaser.Game = new Game(config);
+
+// Add custom scene
+class MyScene extends Scene {
+    constructor() {
+        super("MyScene");
+    }
+}
+game.scene.add("MyScene", MyScene, true);
+
+// Start the game
+game.scene.start("MyScene");
