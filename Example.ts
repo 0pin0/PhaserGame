a0d20c47ffd1ed9f143490ca0729490ed34a712f
0pin0 <67333689+0pin0@users.noreply.github.com>
2023-02-22 16:38:24 +0000

    empty game

--- Example.ts
+++ Example.ts
@@ -1,28 +1,7 @@
-import { Game, Scene } from "phaser";
+import Phaser from 'phaser'
 
-// Define your game configuration
-const config: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO, // Change to Phaser.CANVAS, Phaser.WEBGL, or Phaser.HEADLESS
+const game = new Phaser.Game({
     width: 800,
     height: 600,
-    parent: "game",
-    scene: [], // Add custom scenes to the array
-    physics: {
-        default: "arcade"
-    },
-    backgroundColor: "#000000"
-};
-
-// Create a new game instance based on the configuration
-const game: Phaser.Game = new Game(config);
-
-// Add custom scene
-class MyScene extends Scene {
-    constructor() {
-        super("MyScene");
-    }
-}
-game.scene.add("MyScene", MyScene, true);
-
-// Start the game
-game.scene.start("MyScene");
+    // other game options here
+});
