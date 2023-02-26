12a5241af20a87f1d13c3848d719a604fe456ac0
0pin0 <67333689+0pin0@users.noreply.github.com>
2023-02-26 15:50:14 +0000

    game2 remove phaser

--- src/Game2.ts
+++ src/Game2.ts
@@ -1,23 +1,5 @@
-import "phaser";
-
-export default class Game2 extends Phaser.Scene {
-  constructor() {
-    super("Game2");
-  }
-
-  preload() {}
-
-  create() {
-    console.log("Game2");
-  }
-}
-
-const config = {
-  type: Phaser.AUTO,
-  backgroundColor: "#125555",
-  width: 800,
-  height: 600,
-  scene: Game2,
+const func = () => {
+  console.log(`Game2.ts`);
 };
 
-const game = new Phaser.Game(config);
+func();
