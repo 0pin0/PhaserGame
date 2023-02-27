104c815647a076af177beced17ab5b60eb88d5e7
0pin0 <67333689+0pin0@users.noreply.github.com>
2023-02-27 16:11:30 +0000

    tsc

--- src/Game2.js
+++ src/Game2.js
@@ -1,4 +1,6 @@
 var func = function () {
     console.log("Game2.ts");
+    var el = document.createElement("div");
+    el.innerHTML = "aaa";
 };
 func();
--- src/game.js
+++ src/game.js
@@ -19,27 +19,60 @@ require("phaser");
 var Demo = /** @class */ (function (_super) {
     __extends(Demo, _super);
     function Demo() {
-        return _super.call(this, 'demo') || this;
+        var _this = _super.call(this, "demo") || this;
+        _this.isGame1 = Math.random() > 0.5;
+        return _this;
     }
     Demo.prototype.preload = function () {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+        if (this.isGame1) {
+            this.preload_Game1();
+        }
+        else {
+            this.preload_Game2();
+        }
     };
     Demo.prototype.create = function () {
-        console.log('a');
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-        this.add.image(400, 300, 'libs');
-        var logo = this.add.image(400, 70, 'logo');
+        if (this.isGame1) {
+            this.create_Game1();
+        }
+        else {
+            this.create_Game2();
+        }
+    };
+    Demo.prototype.preload_Game1 = function () {
+        this.load.image("logo", "assets/phaser3-logo.png");
+        this.load.image("libs", "assets/libs.png");
+        this.load.glsl("bundle", "assets/plasma-bundle.glsl.js");
+        this.load.glsl("stars", "assets/starfields.glsl.js");
+    };
+    Demo.prototype.create_Game1 = function () {
+        console.log("Game1");
+        this.add.shader("RGB Shift Field", 0, 0, 800, 600).setOrigin(0);
+        this.add.shader("Plasma", 0, 412, 800, 172).setOrigin(0);
+        this.add.image(400, 300, "libs");
+        var logo = this.add.image(400, 70, "logo");
         this.tweens.add({
             targets: logo,
             y: 350,
             duration: 1500,
-            ease: 'Sine.inOut',
+            ease: "Sine.inOut",
             yoyo: true,
-            repeat: -1
+            repeat: -1,
+        });
+    };
+    Demo.prototype.preload_Game2 = function () {
+        this.load.image("logo", "assets/phaser3-logo.png");
+    };
+    Demo.prototype.create_Game2 = function () {
+        console.log("Game2");
+        var logo = this.add.image(400, 70, "logo");
+        this.tweens.add({
+            targets: logo,
+            y: 350,
+            duration: 1500,
+            ease: "Sine.inOut",
+            yoyo: true,
+            repeat: -1,
         });
     };
     return Demo;
@@ -47,9 +80,9 @@ var Demo = /** @class */ (function (_super) {
 exports.default = Demo;
 var config = {
     type: Phaser.AUTO,
-    backgroundColor: '#125555',
+    backgroundColor: "#125555",
     width: 800,
     height: 600,
-    scene: Demo
+    scene: Demo,
 };
 var game = new Phaser.Game(config);
